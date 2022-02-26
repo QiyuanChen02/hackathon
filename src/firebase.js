43d0aae756a91854556c16ebb09654ec00e09328
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2022-02-26 12:20:59 +0000

    Connected firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyCi5-duEZYABuaHRLOjT2N4km2rVja24Zw',
+    authDomain: 'hackathon-project-c108f.firebaseapp.com',
+    projectId: 'hackathon-project-c108f',
+    storageBucket: 'hackathon-project-c108f.appspot.com',
+    messagingSenderId: '388468059118',
+    appId: '1:388468059118:web:59baf0e97fa80b1dd83cff',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
